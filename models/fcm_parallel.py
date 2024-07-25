e42de82cf7bd66d41cd9c6d7441934e73bc030a3
hung-manh <hmh290204@gmail.com>
2024-07-25 20:00:25 +0700

    fix: fix fcm and fcmp, remove C atribute to be parameter

--- models/fcm_parallel.py
+++ models/fcm_parallel.py
@@ -4,26 +4,25 @@ from .fcm import Dfcm
 
 
 class Dfcm_parallel:
-    def __init__(self, n_jobs: int = None):
-        self._n_jobs = n_jobs if n_jobs is not None else cpu_count()
+    def __init__(self, num_processes: int = None):
+        self._num_processes = num_processes if num_processes is not None else cpu_count() # Số lượng Process tham gia
 
+    def __data_chunks(self, data: np.ndarray):
+        return np.array_split(data, self._num_processes)
+    
     # Thực hiện phân cụm cộng tác mờ song song trên nhiều process.
-    def parallel_cmeans(self, data: np.ndarray, C: int, m: int = 2, epsilon: float = 1e-5, maxiter: int = 1000):
-        n_samples = len(data)   
-        chunk_size = n_samples // self._n_jobs
-        data_chunks = [data[i:i + chunk_size] for i in range(0, n_samples, chunk_size)]
-
-        fcm = Dfcm(C, m, epsilon, maxiter)
+    def parallel_cmeans(self, data: np.ndarray, C: int, m: int = 2, epsilon: float = 1e-5, maxiter: int = 1000,  seed: int = 42):
+        data_chunks = self.__data_chunks(data)
+        fcm = Dfcm(m=m, epsilon=epsilon, maxiter=maxiter)
         
-        with Pool(self._n_jobs) as pool:
+        # Thực hiện phân cụm mờ song song trên nhiều process        
+        with Pool(self._num_processes) as pool:
+            results = pool.starmap(fcm.cmeans, [(chunk, C, seed) for p, chunk in enumerate(data_chunks)]) 
             
-            results = pool.starmap(fcm.cmeans, [(chunk, p+1) for p, chunk in enumerate(data_chunks)]) 
-            
-            u, v, step = zip(*results) 
-            u = np.vstack(u) # Gộp các ma trận thành viên của các process
-            v = fcm.update_cluster_centers(data, u) # Cập nhật tâm cụm từ ma trận thành viên (bat buoc phai gop u xong moi tinh v)
-            
-        return u, v, step
+        # Kết hợp kết quả từ các process
+        all_u = np.concatenate([results[0] for results in results], axis=0)
+        all_v = fcm.update_cluster_centers(data, all_u)            
+        return all_u, all_v
         
             
         
